b32d69ef7cfba339e78489a68cd103821f025c3a
Boris <95616326+zebre13@users.noreply.github.com>
2022-04-14 20:52:53 +0200

    longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,33 +1,42 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @grid = Array.new(10) { ('A'..'Z').to_a.sample }
+    if session[:word_game] != 0
+      @score = session[:word_game]
+    else
+      session[:word_game] = 0
+      @score = session[:word_game]
+    end
   end
-
-  # def score
-  #   raise
-  # end
 
   def score
     if included?
       if english?
-        "Congratulations! #{params[:word].upcase} is a valid English word!"
+        @message = "Congratulations! #{params[:word].upcase} is a valid English word!"
+        session[:word_game] += params[:word].chars.count
+        @score = session[:word_game]
       else
-        "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
+        @message = "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
+        session[:word_game] += 0
+        @score = session[:word_game]
       end
     else
-      "Sorry but #{params[:word].upcase} can't be built out of #{@grid.join(", ")}"
+      @message = "Sorry but #{params[:word].upcase} can't be built out of #{params[:grid].split(" ").join(", ")}"
+      session[:word_game] += 0
+      @score = session[:word_game]
     end
   end
 
   def included?
-    params[:word].chars.all? { |letter| params[:word].count(letter) <= @grid.count(letter) }
+    params[:word].upcase.chars.all? { |letter| params[:word].upcase.count(letter) <= params[:grid].count(letter) }
   end
 
   def english?
-    require 'open-uri'
-    require 'json'
-    response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word].upcase}")
     json = JSON.parse(response.read)
-    json[:found]
+    json['found']
   end
 end
