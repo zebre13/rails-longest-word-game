5bb92797474f2fd08cd553c5538ac78a8277b01c
Boris <95616326+zebre13@users.noreply.github.com>
2022-04-14 15:17:50 +0200

    rails new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,5 +6,15 @@ class GamesController < ApplicationController
   end
 
   def score
+    @message = nil
+    @word = params[:word]
+    rest = @letters - @word.split
+    if rest.length == 10 - @word.split.length
+      @message = "Congratulations! #{@word.upcase} is a valid English word!"
+    elsif rest.length == 11 - @word.split.length
+      @message = "Sorry but #{@word.upcase} does not seem to be a valid English word..."
+    else
+      @message = "Sorry but #{@word.upcase} can't be built out of #{@letters.join(", ")}"
+    end
   end
 end
