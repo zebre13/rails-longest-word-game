09b5bdbe019a69316296ff8d885e5fc5cd3a0962
Boris <95616326+zebre13@users.noreply.github.com>
2022-04-14 18:43:15 +0200

    rails new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,20 +1,33 @@
 class GamesController < ApplicationController
   def new
-    alphabet = ('A'..'Z').to_a
-    @letters = []
-    10.times { @letters << alphabet.sample }
+    @grid = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
+  # def score
+  #   raise
+  # end
+
   def score
-    @message = nil
-    @word = params[:word]
-    rest = @letters - @word.split
-    if rest.length == 10 - @word.split.length
-      @message = "Congratulations! #{@word.upcase} is a valid English word!"
-    elsif rest.length == 11 - @word.split.length
-      @message = "Sorry but #{@word.upcase} does not seem to be a valid English word..."
+    if included?
+      if english?
+        "Congratulations! #{params[:word].upcase} is a valid English word!"
+      else
+        "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
+      end
     else
-      @message = "Sorry but #{@word.upcase} can't be built out of #{@letters.join(", ")}"
+      "Sorry but #{params[:word].upcase} can't be built out of #{@grid.join(", ")}"
     end
+  end
+
+  def included?
+    params[:word].chars.all? { |letter| params[:word].count(letter) <= @grid.count(letter) }
+  end
+
+  def english?
+    require 'open-uri'
+    require 'json'
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    json = JSON.parse(response.read)
+    json[:found]
   end
 end
