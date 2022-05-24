13f7380ac426c93b0e0ee33c1a7374a23300376f
Boris <95616326+zebre13@users.noreply.github.com>
2022-05-24 22:45:14 +0200

    Longest Word Game II

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,12 @@ require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @grid = Array.new(10) { ('A'..'Z').to_a.sample }
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
     if session[:word_game] != 0
       @score = session[:word_game]
     else
@@ -13,29 +17,37 @@ class GamesController < ApplicationController
   end
 
   def score
-    if included?
-      if english?
-        @message = "Congratulations! #{params[:word].upcase} is a valid English word!"
-        session[:word_game] += params[:word].chars.count
-        @score = session[:word_game]
-      else
-        @message = "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
-        session[:word_game] += 0
-        @score = session[:word_game]
-      end
-    else
-      @message = "Sorry but #{params[:word].upcase} can't be built out of #{params[:grid].split(" ").join(", ")}"
-      session[:word_game] += 0
-      @score = session[:word_game]
-    end
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english = english?(@word)
   end
 
-  def included?
-    params[:word].upcase.chars.all? { |letter| params[:word].upcase.count(letter) <= params[:grid].count(letter) }
+  #   if included?
+  #     if english?
+  #       @message = "Congratulations! #{params[:word].upcase} is a valid English word!"
+  #       session[:word_game] += params[:word].chars.count
+  #       @score = session[:word_game]
+  #     else
+  #       @message = "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
+  #       session[:word_game] += 0
+  #       @score = session[:word_game]
+  #     end
+  #   else
+  #     @message = "Sorry but #{params[:word].upcase} can't be built out of #{params[:grid].split(" ").join(", ")}"
+  #     session[:word_game] += 0
+  #     @score = session[:word_game]
+  #   end
+  # end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
-  def english?
-    response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word].upcase}")
+  def english?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
     json = JSON.parse(response.read)
     json['found']
   end
