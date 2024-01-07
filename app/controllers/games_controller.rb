2eebbc13cbd76ab3a63e2dfa4beb7cb586944a56
Boris <95616326+zebre13@users.noreply.github.com>
2024-01-07 17:24:29 +0100

    Installed rest-client gem and built logic to check that word is English

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+
   def new
     alphabet = ("A".."Z").to_a
     @grid = alphabet.sample(10)
@@ -6,5 +7,29 @@ class GamesController < ApplicationController
 
   def score
     @word = params[:word]
+    @grid = params[:grid]
+
+
+    # if from_grid?(@word)
+    #   @message = "Sorry but #{@word.upcase} can't be built out of #{@grid}"
+    # els
+    if english?(@word)
+      @message = "Sorry but #{@word.upcase} doesn't seem to be a valid English word!"
+    else
+      @message = "Congratulations! #{@word.upcase} is a valid English word!"
+    end
+  end
+
+  private
+
+  def from_grid?(word)
+
+  end
+
+  def english?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word.downcase}"
+    response = RestClient.get url
+    result = JSON.parse(response)
+    return result["found"]
   end
 end
