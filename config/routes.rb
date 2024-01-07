e45fefb853b7881d05fa6c581902499922c799c8
Boris <95616326+zebre13@users.noreply.github.com>
2024-01-07 18:25:12 +0100

    Add link to new game on score view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  get 'games/score'
+  # get 'games/new'
+  # get 'games/score'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
