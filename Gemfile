39f6d0208c3b504abd109cf29fdbd900dcd2c8c4
Boris <95616326+zebre13@users.noreply.github.com>
2024-01-11 19:28:50 +0100

    Remove RestClient gem and replace grid with letters naming

--- Gemfile
+++ Gemfile
@@ -45,8 +45,6 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 # gem "sassc-rails"
 
-gem "rest-client"
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,21 +2,21 @@ class GamesController < ApplicationController
 
   def new
     alphabet = ("A".."Z").to_a
-    @grid = alphabet.sample(10)
+    @letters = alphabet.sample(10)
   end
 
   def score
     @word = params[:word].upcase
-    @grid = params[:grid].upcase
-    @included = included?(@word, @grid)
+    @letters = params[:letters].split
+    @included = included?(@word, @letters)
     @english = english?(@word)
   end
 
   private
 
-  def included?(word, grid)
+  def included?(word, letters)
     word.chars.all? do |letter|
-      word.count(letter) <= grid.split(", ").count(letter)
+      word.count(letter) <= letters.count(letter)
     end
   end
 
@@ -25,9 +25,5 @@ class GamesController < ApplicationController
     response = URI.open(url)
     json = JSON.parse(response.read)
     json["found"]
-    # url = "https://wagon-dictionary.herokuapp.com/#{word.downcase}"
-    # response = RestClient.get url
-    # result = JSON.parse(response)
-    # return result["found"]
   end
 end
