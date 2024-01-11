cb3dd5224c0b4ad4667dff7c551f913ae184a63c
Boris <95616326+zebre13@users.noreply.github.com>
2024-01-11 21:16:29 +0100

    Persist score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,10 @@
 require "open-uri"
 
 class GamesController < ApplicationController
-  
+
   def new
     alphabet = ("A".."Z").to_a
     @letters = alphabet.sample(10)
-    session[:score] = 0
   end
 
   def score
